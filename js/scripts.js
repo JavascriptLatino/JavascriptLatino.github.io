fa0796346bcab8226197daa892d2bd83162f663b
Daniel Herrera <admin@digammastudio.com>
2016-07-28 16:42:42 -0500

    Add NodeSchool Events

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+jQuery('document').ready(function ($) {
+    $('#slide-events').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        infinite: false,
+    });
+})
